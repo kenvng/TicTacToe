00b81af5244347321b25c73ed78cd87bb9dc060b
vuken <kenvng@gmail.com>
2016-02-07 18:03:07 -0500

    Sixth commit. unable to show scores and turn

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -12,31 +12,60 @@
 
  $(document).ready(() => {
 
-   // SIGN UP
+
+   // SIGN UP //
    $('#sign-up').on('submit', function(e) {
      e.preventDefault();
      var formData = new FormData(e.target);
      $.ajax({
        url: myApp.baseUrl + '/sign-up',
        method: 'POST',
+      //  headers: {
+      //    Authorization: 'Token token=' + myApp.user.token,
+      //  },
        contentType: false,
        processData: false,
        data: formData,
      }).done(function(data) {
+      //  myApp.user = data.user;
        console.log(data);
      }).fail(function(jqxhr) {
        console.error(jqxhr);
      });
    });
 
-   // SIGN IN
+  //  $('.sign-up').on('submit',function(event) {
+  //    event.preventDefault();
+  //    let submtData = new FormData(event.target);
+  //    $.ajax({
+  //      url: myApp.baseUrl + '/sign-up',
+  //      type: 'POST',
+  //      contentType: false,
+  //      processData: false,
+  //      data: submtData
+  //    })
+  //    .done(function(data) {
+  //        console.log(data);
+  //        $('.sign-up-form').hide();
+  //        $('.sign-up-nav').hide();
+  //    }).fail(function(jqxhr) {
+  //        console.error(jqxhr);
+  //    });
+  //  });
+
+   // SIGN IN //
    $('#sign-in').on('submit', function(e) {
      e.preventDefault();
      var formData = new FormData(e.target); // grab FormData
+
      $.ajax({
-       //url: 'http://httpbin.org/post',
-       url: myApp.baseUrl + '/sign-in',
+      //  url: 'http://httpbin.org/post',
+      //  url: myApp.baseUrl + '/sign-in',
+       url: myApp.baseUrl + '/sign-in/' + myApp.user.id,
        method: 'POST',
+      //  headers: {
+      //    Authorization: 'Token token=' + myApp.user.token,
+      //  },
        contentType: false,
        processData: false,
        data: formData,
@@ -48,37 +77,40 @@
      });
    });
 
-   // SIGN OUT
-   $('#sign-out').on('submit', function(e) {
+
+  // $('.sign-in').on('submit',function(event) {
+  //   event.preventDefault();
+  //   let data = new FormData(event.target);
+  //   $.ajax({
+  //     url: myApp.baseUrl + '/sign-in',
+  //     type: 'POST',
+  //     contentType: false,
+  //     processData: false,
+  //     data: data
+  //   })
+  //   .done(function(data) {
+  //       console.log(data);
+  //       $('.sign-in').hide();
+  //       $('.sign-up').hide();
+  //       myApp.user = data.user;
+  //   }).fail(function(jqxhr) {
+  //       console.error(jqxhr);
+  //   });
+  // });
+
+
+   // SIGN OUT //
+   $('#sign-out').on('click', function(e) {
      e.preventDefault();
      var formData = new FormData(e.target); // grab FormData
-     $.ajax({
-       //url: 'http://httpbin.org/post',
-       url: myApp.baseUrl + '/sign-out/:id',
-       method: 'DELETE',
-       contentType: false,
-       processData: false,
-       data: formData,
-     }).done(function(data) {
-       myApp.user = data.user;
-       console.log(data);
-     }).fail(function(jqxhr) {
-       console.error(jqxhr);
-     });
-   });
 
-   // CHANGE PASSWORD
-   $('#change-password').on('submit', function(e) {
-     e.preventDefault(); // prevent browser do what it normally do.
-     if (!myApp.user) {
-       console.error('Wrong!');
-     }
-     var formData = new FormData(e.target); // grab FormData
      $.ajax({
-       //url: 'http://httpbin.org/post',
-       url: myApp.baseUrl + '/change-password/:id' + myApp.user.id,
-       method: 'PATCH',
+      //  url: 'http://httpbin.org/post',
+      //  url: myApp.baseUrl + '/sign-out/:id',
+       url: myApp.baseUrl + '/sign-out/' + myApp.user.id,
+       method: 'DELETE',
        headers: {
+        //  Authorization: 'Token token=' + '/sign-out/:token',
          Authorization: 'Token token=' + myApp.user.token,
        },
        contentType: false,
@@ -86,11 +118,81 @@
        data: formData,
      }).done(function(data) {
        myApp.user = data.user;
+       console.log(data);
+     }).fail(function(jqxhr) {
+       console.error(jqxhr);
+     });
+   });
+
+
+  // $('.sign-out').on('click', function(e) {
+  //    e.preventDefault();
+  //    $.ajax({
+  //      url: myApp.baseUrl + '/sign-out/' + myApp.user.id,
+  //      method: 'DELETE',
+  //      headers: {
+  //        Authorization: 'Token token=' + myApp.user.token
+  //      },
+  //    })
+  //     .done(function() {
+  //      myApp.user.token = null;
+  //      $('.sign-in').show();
+  //      $('.sign-up').show();
+  //      console.log(myApp.user);
+  //    }).fail(function(jqxhr) {
+  //      console.error(jqxhr);
+  //    });
+  //  });
+
+
+   // CHANGE PASSWORD //
+   $('#change-password').on('submit', function(e) {
+     e.preventDefault(); // prevent browser do what it normally do.
+     if (!myApp.user) {
+       console.error('Wrong!');
+     }
+     var formData = new FormData(e.target); // grab FormData
+
+     $.ajax({
+       //url: 'http://httpbin.org/post',
+       url: myApp.baseUrl + '/change-password/' + myApp.user.id,
+       method: 'PATCH',
+       headers: {
+         Authorization: 'Token token=' + myApp.user.token,
+       },
+       contentType: false,
+       processData: false,
+       data: formData,
+     }).done(function() {
+      //  myApp.user = data.user;
        console.log('Successfully change password');
      }).fail(function(jqxhr) {
        console.error(jqxhr);
      });
    });
+
+
+  // $('.change-password').on('submit', function(e) {
+  //    e.preventDefault(); // prevent browser do what it normally do.
+  //    if (!myApp.user) {
+  //      console.error('Wrong!');
+  //    }
+  //    var formData = new FormData(e.target); // grab FormData
+  //    $.ajax({
+  //      url: myApp.baseUrl + '/change-password/' + myApp.user.id,
+  //      method: 'PATCH',
+  //      headers: {
+  //        Authorization: 'Token token=' + myApp.user.token,
+  //      },
+  //      contentType: false,
+  //      processData: false,
+  //      data: formData,
+  //    }).done(function() {
+  //      console.log('Successfully change password');
+  //    }).fail(function(jqxhr) {
+  //      console.error(jqxhr);
+  //    });
+  //  });
  });
 
 
